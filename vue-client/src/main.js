aeb1ff9707d8819a4ea6583e553d57043531ccb6
CornelCodes <cornelcodes@gmail.com>
2021-05-02 13:44:27 +0200

    Updated Dependencies Vue 3.0.11 axios vue-axios

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
-import VueAxios from 'vue-axios'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+window.axios = axios
+axios.defaults.baseURL = 'https://localhost:5001'
 
 new Vue({
   render: h => h(App),
