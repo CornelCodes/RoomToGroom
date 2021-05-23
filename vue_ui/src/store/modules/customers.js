e1f247bcba3fbca165f89fd582df24ce7790ff0e
CornelCodes <cornelcodes@gmail.com>
2021-05-23 12:05:35 +0200

    Vuex state refactored to ensure single source of truth throughout client application

--- vue_ui/src/store/modules/customers.js
+++ vue_ui/src/store/modules/customers.js
@@ -1,15 +1,58 @@
+import axios from "axios"
 
 export default {
     namespaced: true,
     state: {
-        customers: []
+        userCustomers: []
     },
 
     mutations: {
-
+        SET_CUSTOMERS(state, customers)
+        {
+            state.userCustomers = customers
+            console.log(customers)
+        }
     },
 
     actions: {
+        async getAllCustomers({ commit })
+        {
 
+            await axios.get('api/Customer/GetAll', {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .then(res =>
+                {
+                    console.log('Success getting customers')
+                    commit('SET_CUSTOMERS', res.data)
+                })
+                .catch(err =>
+                {
+                    console.log(err)
+                })
+        },
+
+        createCustomer()
+        {
+            axios.post("api/customer", this.newCustomer, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            })
+                .then((res) =>
+                {
+                    console.log("Success creating customer");
+                    this.showCreateCustomer = false;
+                    this.getCustomers();
+                    //Refresh list data
+                })
+                .catch((err) =>
+                {
+                    console.log(err);
+                });
+
+        }
     }
 }
--- vue_ui/src/store/modules/pets.js
+++ vue_ui/src/store/modules/pets.js
@@ -1,15 +1,36 @@
+import axios from "axios";
 
 export default {
     namespaced: true,
     state: {
-
+        userPets: [],
     },
 
     mutations: {
-
+        SET_PETS(state, pets)
+        {
+            state.userPets = pets;
+            console.log(pets)
+        }
     },
 
     actions: {
-
+        async getAllPets({ commit })
+        {
+            await axios.get('api/Pet/GetAll', {
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .then(res =>
+                {
+                    commit('SET_PETS', res.data)
+                    console.log('Success getting pets')
+                })
+                .catch(err =>
+                {
+                    console.log(err)
+                })
+        },
     }
 }
